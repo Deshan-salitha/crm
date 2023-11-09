550308814b5e1cb4f0c921f4b5010146891608c4
Deshan-salitha <deshan.salitha@outlook.com>
2023-11-09 23:28:14 +0530

    registration done

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -8,7 +9,7 @@ def home(request):
     # check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
+        password = request.POST.get('password')
         #     Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -29,3 +30,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out!'))
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username, password)
+            login(request, user)
+            messages(request, ('You have registered!'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
